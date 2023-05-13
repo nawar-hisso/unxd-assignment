f991f96a58a2796c2fb4f9b460a47ba1a99597e8
nawar-hisso <nawar@polytrade.finance>
2023-05-13 23:03:53 +0400

    :ambulance: Fix Local & Dev Environment

--- src/Configs/Wallet.js
+++ src/Configs/Wallet.js
@@ -4,12 +4,11 @@ const CONNECT_REQUEST_METHOD = 'eth_requestAccounts';
 const CHANGE_WALLET_EVENT = 'accountsChanged';
 const CANCEL_CODE = 4001;
 const { REDIRECT_URI } = CONSTANTS;
-const METAMASK_MOBILE_BROWSER =
-  'https://metamask.app.link/dapp/unxd.netlify.app/';
+const METAMASK_MOBILE_BROWSER = `https://metamask.app.link/dapp/${REDIRECT_URI}/`;
 const ETHEREUM_CHAIN_ID = 1;
-const GOERLI_CHAIN_ID = 1;
+const GOERLI_CHAIN_ID = 5;
 const CHAIN_ID =
-  CONSTANTS.ENV === CONSTANTS.LOCAL_ENV ? ETHEREUM_CHAIN_ID : GOERLI_CHAIN_ID;
+  CONSTANTS.ENV === CONSTANTS.LOCAL_ENV ? GOERLI_CHAIN_ID : ETHEREUM_CHAIN_ID;
 
 const WALLET = {
   CONNECT_REQUEST_METHOD,
