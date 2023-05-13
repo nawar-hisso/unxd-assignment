c2ab00800336ff882ce359b8816fa9e61373fd4f
nawar-hisso <nawar@polytrade.finance>
2023-05-13 18:37:14 +0400

    :recycle: Change Store Wrapped Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Home from './Components/Templates/Home';
 import ROUTES_NAMES from './Configs/RoutesNames';
-import Store from './Store';
+import { changeWalletListener } from './Actions/MetaMask';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    changeWalletListener(dispatch);
+  }, [dispatch]);
+
   return (
-    <Store>
-      <Router>
-        <Routes>
-          <Route path={ROUTES_NAMES.HOME} element={<Home />} />
-        </Routes>
-      </Router>
-    </Store>
+    <Router>
+      <Routes>
+        <Route path={ROUTES_NAMES.HOME} element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
 
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,11 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import Store from './Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Store>
+    <App />
+  </Store>,
+);
