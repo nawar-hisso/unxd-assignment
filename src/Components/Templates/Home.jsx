916ac107b94834465808a634b99d82104d5bd77f
nawar-hisso <nawar@polytrade.finance>
2023-05-13 18:36:47 +0400

    :zap: Call MetaMask Connect Function

--- src/Components/Templates/Home.jsx
+++ src/Components/Templates/Home.jsx
@@ -2,14 +2,22 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Button } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import { connectToMetaMask } from '../../Actions/MetaMask';
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  const connectWallet = async () => {
+    await connectToMetaMask(dispatch);
+  };
+
   return (
     <Container>
       <Row className="justify-content-md-center">
         <Col md="12" className="text-center">
           <h1>UNXD Home Page</h1>
-          <Button>Click me</Button>
+          <Button onClick={connectWallet}>Connect Wallet</Button>
         </Col>
       </Row>
     </Container>
